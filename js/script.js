1f10e1b60da5a260829b0a3f1c57787d750c5333
Hannah Brown <nagle.hannah1@gmail.com>
2021-07-21 18:57:27 -0400

    commit on 'contributor'

--- js/script.js
+++ js/script.js
@@ -118,23 +118,3 @@ document
   .addEventListener("click", buttonClickHandler);
 
 document.getElementById("motivation").addEventListener("click", getMotivated);
-
-$errors = '';
-$myemail = 'projectonefeelingapp@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors = "\n Error: all fields are required";
-}
-
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors = "\n Error: Invalid email address";
-}

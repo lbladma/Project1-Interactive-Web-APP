2f0cad9ca6a7974391e2cfe129884a9014564e8f
Hannah Brown <nagle.hannah1@gmail.com>
2021-07-21 18:54:55 -0400

    commit on 'contributor'

--- js/script.js
+++ js/script.js
@@ -110,9 +110,31 @@ var buttonClickHandler = function (event) {
     getLocations(feelings);
   }
 };
+var instance = M.Modal.getInstance(elem);
+
 
 document
   .getElementById("sidebar")
   .addEventListener("click", buttonClickHandler);
 
 document.getElementById("motivation").addEventListener("click", getMotivated);
+
+$errors = '';
+$myemail = 'projectonefeelingapp@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors = "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors = "\n Error: Invalid email address";
+}
